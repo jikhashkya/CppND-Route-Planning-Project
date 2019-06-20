d620250bcd562431913c279e4a81c7976783189e
Pramesh Shakya <prmshakya@gmail.com>
2019-06-19 20:55:28 -0400

    Update route_model.h

--- src/route_model.h
+++ src/route_model.h
@@ -45,7 +45,7 @@ class RouteModel : public Model {
   	std::vector<Node>& SNodes(){return m_Nodes;}
   
     // public getter function to get node_to_road information
-  	std::unordered_map<int,std::vector<const Model::Road *>> & GetNodeToRoadMap(){return node_to_road;}
+    std::unordered_map<int,std::vector<const Model::Road *>> & GetNodeToRoadMap(){return node_to_road;}
     
     RouteModel::Node& FindClosestNode(float x, float y);
     
@@ -54,9 +54,9 @@ class RouteModel : public Model {
   private:
     // Add private RouteModel variables and methods here.
     std::vector<Node> m_Nodes{};   // stores all nodes from Open Street Map data
-   	std::unordered_map<int, std::vector<const Model::Road *>> node_to_road;
+    std::unordered_map<int, std::vector<const Model::Road *>> node_to_road;
   
-  	//private member function
-  	void CreateNodeToRoadHashMap();
+    //private member function
+    void CreateNodeToRoadHashMap();
 
 };
