013e317098dc708761f1ebeb7a8a4294fad7a1c4
Pramesh Shakya <prmshakya@gmail.com>
2019-06-19 20:49:31 -0400

    Update route_model.h

--- src/route_model.h
+++ src/route_model.h
@@ -12,11 +12,11 @@ class RouteModel : public Model {
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
-        Node *parent = nullptr;
+        RouteModel::Node *parent = nullptr;
       	float h_value = std::numeric_limits<float>::max();
       	float g_value = 0.0;
       	bool visited = false;
-      	std::vector<Node*> neighbors;
+      	std::vector<RouteModel::Node*> neighbors;
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
