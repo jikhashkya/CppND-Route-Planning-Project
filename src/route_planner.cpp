a545d4a063bcd05cd91f97e04f2d25c6bb1d78a9
Pramesh Shakya <prmshakya@gmail.com>
2019-06-21 13:27:22 -0400

    Corrected two end_y occurrences to end_x
    This error was not helping with creating the path

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -6,7 +6,7 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     //scaling 
   	start_x *= 0.01;
   	start_y *= 0.01;
-  	end_y *= 0.01;
+  	end_x *= 0.01;
   	end_y *= 0.01;
   
     start_node = &m_Model.FindClosestNode(start_x, start_y);
